649c7faa10ce1fd45272ef873bbbb1b8190ef005
dArKrEsQ <razvan.bielz@gmail.com>
2022-04-23 14:41:52 +0300

    finished with class components

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Card from "../card/card.component";
+import "./card-list.styles.css";
+
+class CardList extends Component {
+  render() {
+    const { filteredMonsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {filteredMonsters.map((monster) => (
+          <Card monster={monster} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
